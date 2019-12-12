98198c3100bcb29d2e8510c8cc5ae2f24abda812
Николай Смольянинов <smolianinow.colya2016@yandex.ru>
2019-12-12 19:10:11 +0300

    откат

--- js/jQuery.js
+++
@@ -1,6 +0,0 @@
-$(window).on('load', function () {
-    var $preloader = $('#page-preloader'),
-        $spinner   = $preloader.find('.spinner');
-    $spinner.fadeOut();
-    $preloader.delay(350).fadeOut('slow');
-});
